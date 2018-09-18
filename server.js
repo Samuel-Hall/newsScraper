2302fefe724d903ffd7dd621af7d0d5ed7578832
Samuel-Hall <samuel.r.hall14@gmail.com>
2018-09-17 21:45:53 -0400

    mongo

--- server.js
+++ server.js
@@ -27,10 +27,14 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
-mongoose.connect(
-  "mongodb://localhost/newsScraperDB",
-  { useNewUrlParser: true }
-);
+// If deployed, use the deployed database. Otherwise use the local newsScraperDB database
+var MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/newsScraperDB";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Routes
 app.get("/", function(req, res) {
